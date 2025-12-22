9f1472470b61a46e96c37b75dd7d949509c67fd8
Fabii1995 <fabikaufman9@gmail.com>
2025-12-22 23:20:22 +0200

    added navigation solved custom button thing

---
+++ app/src/main/java/com/example/SlothGaming/Ui/AddReviewFragment.kt
@@ -0,0 +1,6 @@
+package com.example.SlothGaming.Ui
+
+import androidx.fragment.app.Fragment
+
+class AddReviewFragment : Fragment() {
+}
--- app/src/main/java/com/example/SlothGaming/Ui/MyReviewsFragment.kt
+++ app/src/main/java/com/example/SlothGaming/Ui/MyReviewsFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.R
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.SlothGaming.Ui.view_models.ReviewViewModelFactory
 import com.example.SlothGaming.data.repository.ReviewListRepository
 import com.example.SlothGaming.databinding.MyReviewsLayoutBinding
@@ -27,10 +29,9 @@ class MyReviewsFragment : Fragment() {
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.addReviewButton.root.setOnClickListener {_ ->
+        }
 
-//        binding.addReviewButton.setOnClickListener {
-//            // TODO: handle add review click
-//        }
     }
 
     override fun onDestroyView() {
