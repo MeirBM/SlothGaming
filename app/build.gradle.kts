a8c1822e62fe38012101e8834d8fafde7d92de4c
Fabii1995 <fabikaufman9@gmail.com>
2026-01-11 18:17:19 +0200

    Added extensions and linked to fire base

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.gms.google-services")
     id("kotlin-kapt")
     id("org.jetbrains.kotlin.plugin.parcelize")
 }
@@ -15,7 +16,7 @@ android {
     }
     defaultConfig {
         applicationId = "com.example.mainproject"
-        minSdk = 24
+        minSdk = 28
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
@@ -56,6 +57,13 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation( "androidx.lifecycle:lifecycle-viewmodel-ktx:2.10.0")
+    //coroutines dependencies
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
+    //fire base dependencies
+    implementation(platform("com.google.firebase:firebase-bom:34.7.0"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
+
 }
