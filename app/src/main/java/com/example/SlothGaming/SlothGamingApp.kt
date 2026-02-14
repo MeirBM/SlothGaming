e1f4c5bf10a3315fce94974d70a4c0fffbd94dff
talmagor <tala08455@gmail.com>
2026-02-15 00:00:36 +0200

    Update SlothGamingApp.kt

--- app/src/main/java/com/example/SlothGaming/SlothGamingApp.kt
+++ app/src/main/java/com/example/SlothGaming/SlothGamingApp.kt
@@ -1,6 +1,7 @@
 package com.example.SlothGaming
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.SlothGaming.utils.NotificationHelper
 import dagger.hilt.android.HiltAndroidApp
 
@@ -8,6 +9,7 @@ import dagger.hilt.android.HiltAndroidApp
 class SlothGamingApp : Application() {
     override fun onCreate() {
         super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         NotificationHelper.createChannel(this)
     }
 }
