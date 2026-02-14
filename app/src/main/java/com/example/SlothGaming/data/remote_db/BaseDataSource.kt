93a27e360ec18ca59b5af6f4f1ce22ed8c81af81
talmagor <tala08455@gmail.com>
2026-02-14 21:19:59 +0200

    fix network error

--- app/src/main/java/com/example/SlothGaming/data/remote_db/BaseDataSource.kt
+++ app/src/main/java/com/example/SlothGaming/data/remote_db/BaseDataSource.kt
@@ -3,6 +3,7 @@ package com.example.SlothGaming.data.remote_db
 import android.util.Log
 import com.example.SlothGaming.utils.Resource
 import retrofit2.Response
+import kotlin.coroutines.cancellation.CancellationException
 
 abstract class BaseDataSource {
 
@@ -19,6 +20,8 @@ abstract class BaseDataSource {
             }
             return Resource.error("Network call has failed for the following reason: " +
                     "${result.message()} ${result.code()}")
+        }catch (e : CancellationException) {
+            throw e
         }catch (e : Exception) {
             return Resource.error("Network call has failed for the following reason: "
                     + (e.localizedMessage ?: e.toString()))
